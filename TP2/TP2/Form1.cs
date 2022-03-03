16e6c4b8b74e690f83314ead772512ef59e254d9
OrvalaAM <90945844+OrvalaAM@users.noreply.github.com>
2022-03-04 02:35:58 +0700

    tambahan fitur GUI

--- TP2/TP2/Form1.cs
+++ TP2/TP2/Form1.cs
@@ -16,5 +16,10 @@ namespace TP2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hai, " + textBox1.Text;
+        }
     }
 }
